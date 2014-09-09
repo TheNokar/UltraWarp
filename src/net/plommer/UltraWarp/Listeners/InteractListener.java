4dba1b3a663fe3397ef6066ec3ddadcede6e61bf
TheNokar <nokar1@live.com>
2014-09-09 14:54:44 +0000

    Added pages to the warp compass and some bug fixes

--- src/net/plommer/UltraWarp/Listeners/InteractListener.java
+++ src/net/plommer/UltraWarp/Listeners/InteractListener.java
@@ -2,7 +2,6 @@ package net.plommer.UltraWarp.Listeners;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 import net.plommer.UltraWarp.IconMenu;
 import net.plommer.UltraWarp.UltraWarp;
@@ -26,36 +25,74 @@ public class InteractListener implements Listener {
 	public InteractListener(UltraWarp plugin) {
 		this.plugin = plugin;
 	}
-	final HashMap<Integer, List<Warps>> wp = new HashMap<Integer, List<Warps>>();
+	int page = 1;
+
 	@EventHandler
 	public void interActEvent(PlayerInteractEvent event) {
 		if(event.getAction().equals(Action.PHYSICAL) || event.getAction().equals(Action.RIGHT_CLICK_AIR)  || event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
 			final Player player = event.getPlayer();
 			if(event.getItem().getItemMeta().equals(UsefullItems.WarpCompass().getItemMeta())) {
-		    	this.menus(player, "My Warps");
+				page = 1;
+				new getWarps(plugin);
+				HashMap<Integer, ArrayList<Warps>> wa = getWarps.getWarItemPos(player);
+    			createMenu(player, "My Warps", wa, 1);
 			}
 		}
 	}
 	
-	public IconMenu menus(final Player player, final String name) {
-		IconMenu menu = this.menus(player, name);
-		menu.setOption(18, new ItemStack(Material.ARROW, 1), "Back", "Back");
-    	menu.setOption(26, new ItemStack(Material.ARROW, 1), "Next", "Next");
-    	new getWarps(plugin);
-		HashMap<Integer, ArrayList<Warps>> wa = getWarps.getWarItemPos(player);
-		int pos = 0;
-		for(Warps w : wa.get(1)) {
-			menu.setOption(pos, new ItemStack(Material.PAPER, 1), w.getWarpName());
-			pos++;
-		}
-		menu.open(player);
+	public IconMenu menus(final Player player, final String name, final HashMap<Integer, ArrayList<Warps>> wa) {
 		return new IconMenu(ChatColor.GOLD + name, 27, new IconMenu.OptionClickEventHandler() {
             @Override
             public boolean onOptionClick(IconMenu.OptionClickEvent event) {
-            	WarpPlayer.playerTo(event.getName(), player, plugin);
+            	if(event.getName().equalsIgnoreCase("next")) {
+            		if(wa.containsKey(page+1)) {
+            			page++;
+            			createMenu(player, "My Warps", wa, page);
+            		}
+            		event.setWillClose(false);
+            		event.setWillDestroy(true);
+            	} else if(event.getName().equalsIgnoreCase("back")) {
+            		if(wa.containsKey(page-1)) {
+            			page--;
+            			createMenu(player, "My Warps", wa, page);
+            		}
+            		event.setWillClose(false);
+            		event.setWillDestroy(true);
+            	} else if(event.getName().equalsIgnoreCase("no")) {
+            		return false;
+            	} else {
+            		WarpPlayer.playerTo(event.getName(), player, plugin);
+            		event.setWillClose(true);
+            		event.setWillDestroy(true);
+            		return false;
+            	}
+            	event.setWillClose(false);
             	return false;
             }
         }, plugin);
+	}
+
+	public void setMenu(IconMenu menu, Warps w, int pos, Material mat) {
+		String v;
+		if(!w.isPublic()) {
+			v = ChatColor.RED + "Private";
+		} else {
+			v = ChatColor.GREEN + "Public";
+		}
+		menu.setOption(pos, new ItemStack(mat, 1), w.getWarpName(), new String[] {ChatColor.AQUA + "Location: " + ChatColor.YELLOW + (int)w.getLocation()[0] + ", " + (int)w.getLocation()[1] + ", " + (int)w.getLocation()[3], ChatColor.AQUA + "Status: " + v});
+	}
+	
+	public void createMenu(Player player, String name, HashMap<Integer, ArrayList<Warps>> wa, int page) {	
+		IconMenu menu = this.menus(player, name, wa);
+		menu = menus(player, name, wa);
+    	menu.setOption(18, new ItemStack(Material.ARROW, 1), ChatColor.RED + "Back");
+	    menu.setOption(26, new ItemStack(Material.ARROW, 1), ChatColor.GREEN + "Next");
+		int pos = 0;
+		for(Warps w : wa.get(page)) {
+			setMenu(menu, w, pos, Material.PAPER);
+			pos++;
+		}
+		menu.open(player);
 	}
 	
 }
